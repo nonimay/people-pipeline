193b3517509fc65819ecafccb4720a3bd46a3fe6
Fiona McDonald <fiona.mcdonald@digital.hmrc.gov.uk>
2017-02-14 18:53:11 +0000

    added password digest to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
     presence: true,
     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
     uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
+  it { should validate_length_of(:password).is_at_least(6) }
 
   describe 'email validation' do
 
     before :each do
-      @user = User.new(name: 'John Smith', email: 'john.smith@test.com')
+      @user = User.new(name: 'John Smith', email: 'john.smith@test.com', password: "foobar", password_confirmation: "foobar")
       @user.save
     end
 
@@ -30,7 +32,8 @@ RSpec.describe User, type: :model do
     end
 
     it 'should always save emails as downcase' do
-      user2 = User.new(name: 'Jane Smith', email: 'Jane.Smith@test.com')
+      user2 = @user.dup
+      user2.email = 'Jane.Smith@test.com'
       user2.save
       expect(user2.email).to eq('jane.smith@test.com')
     end
