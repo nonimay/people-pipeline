34f63af41814a22155a1efb3f4c8a71f564df73a
Fiona McDonald <fiona.mcdonald@digital.hmrc.gov.uk>
2017-02-20 21:40:22 +0000

    login page and controller

--- Gemfile
+++ Gemfile
@@ -31,7 +31,6 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'rspec-rails'
 
 gem 'pry'
-gem 'bootstrap'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'login', to: 'sessions#new'
+
   resources :candidates
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
